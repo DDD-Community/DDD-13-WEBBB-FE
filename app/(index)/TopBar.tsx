384cbc510fb9da527edef5ea30713adccad853be
Seohyun Roh <radult951@gmail.com>
2026-05-27 04:24:57 +0900

    feat: 홈 햄버거 메뉴 구현

---
+++ app/(index)/TopBar.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import AlarmDefault from "@/assets/icons/ic_alarm_default.svg";
+import Menu from "@/assets/icons/ic_menu.svg";
+import Close from "@/assets/icons/ic_close.svg";
+import { useState } from "react";
+
+export default function TopBar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const handleMenuClick = () => {
+    setIsMenuOpen((prev) => !prev);
+  };
+
+  return (
+    <>
+      <div className="sticky top-0 z-10 flex h-17 items-center justify-between bg-black px-4">
+        <p className="text-head-22sb">서비스 로고</p>
+
+        <div className="align-center flex gap-4">
+          <button type="button">
+            <AlarmDefault className="h-6 w-6 flex-none text-gray-50" />
+          </button>
+          <button type="button" onClick={handleMenuClick}>
+            {isMenuOpen ? (
+              <Close className="h-6 w-6 flex-none text-white" />
+            ) : (
+              <Menu className="h-6 w-6 flex-none text-gray-50" />
+            )}
+          </button>
+        </div>
+      </div>
+
+      {isMenuOpen && (
+        <>
+          <div className="fixed inset-0 z-20" onClick={handleMenuClick} />
+          <div className="text-body-16m text-gray-10 absolute top-17 z-20 flex w-full flex-col gap-6 bg-black px-6 py-5">
+            <a href="#">홈</a>
+            <a href="#">마이페이지</a>
+            <a href="#">설정</a>
+          </div>
+        </>
+      )}
+    </>
+  );
+}
--- app/(index)/page.tsx
+++ app/(index)/page.tsx
@@ -1,7 +1,5 @@
-import AlarmDefault from "@/assets/icons/ic_alarm_default.svg";
-import Menu from "@/assets/icons/ic_menu.svg";
-
 import Filter from "./Filter";
+import TopBar from "./TopBar";
 import WriteButton from "./WriteButton";
 import CharacterCard from "@/components/CharacterCard";
 
@@ -9,19 +7,7 @@ export default function Home() {
   return (
     <>
       <div>
-        <div className="sticky top-0 z-10 flex h-17 items-center justify-between bg-black px-4">
-          <p className="text-head-22sb">서비스 로고</p>
-
-          <div className="align-center flex gap-4">
-            <button type="button">
-              <AlarmDefault className="h-6 w-6 flex-none text-gray-50" />
-            </button>
-            <button type="button">
-              <Menu className="h-6 w-6 flex-none text-gray-50" />
-            </button>
-          </div>
-        </div>
-
+        <TopBar />
         <Filter />
       </div>
 
