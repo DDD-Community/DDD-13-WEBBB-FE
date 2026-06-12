ea132e3cc3187509a249a70e5a465183ef7544a2
박하은 <hani00415@naver.com>
2026-06-13 01:36:19 +0900

    [Chore] 말풍선 컴포넌트 미세 조정

--- app/post/[id]/SpeechBubble.tsx
+++ app/post/[id]/SpeechBubble.tsx
@@ -8,14 +8,15 @@ interface SpeechBubbleProps {
 
 export default function SpeechBubble({ text }: SpeechBubbleProps) {
   return (
-    <div className="border-gray-80 relative flex h-6 items-center rounded-[4px] border bg-black pr-2.5 pl-4">
-      <div className="border-gray-80 absolute top-1/2 -left-[4px] h-[7px] w-[7px] -translate-y-1/2 rotate-45 border-b border-l bg-black" />
+    <div className="border-gray-80 relative flex items-center rounded-[4px] border bg-black py-[2px] pr-2 pl-2">
+      <div className="border-gray-80 absolute top-1/2 -left-[4.5px] h-2 w-2 -translate-y-1/2 rotate-45 border-b border-l bg-black" />
 
       <span
         className="text-gray-40 z-10 font-medium"
         style={{
+          fontFamily: "Pretendard",
           fontSize: "13px",
-          lineHeight: "150%",
+          lineHeight: "180%",
           letterSpacing: "-0.26px",
         }}
       >
