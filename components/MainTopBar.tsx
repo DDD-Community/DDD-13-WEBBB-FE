e1d6564ddc9c18e2e3576316477ac5d3d5213847
Seohyun <radult951@gmail.com>
2026-06-21 14:52:15 +0900

    fix: 로그인 상태에서만 알림,메뉴 아이콘 표시 (#32)

--- components/MainTopBar.tsx
+++ components/MainTopBar.tsx
@@ -3,12 +3,19 @@
 import AlarmDefault from "@/assets/icons/ic_alarm_default.svg";
 import Menu from "@/assets/icons/ic_menu.svg";
 import X from "@/assets/icons/ic_x.svg";
+import { useAuthStore } from "@/store/useAuthStore";
 import { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 
 export default function MainTopBar() {
+  const user = useAuthStore((state) => state.user);
+  const hasHydrated = useAuthStore((state) => state._hasHydrated);
+
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const isLoggedIn = hasHydrated && Boolean(user);
+  const shouldShowMenu = isLoggedIn && isMenuOpen;
 
   const handleMenuClick = () => {
     setIsMenuOpen((prev) => !prev);
@@ -19,21 +26,23 @@ export default function MainTopBar() {
       <div className="sticky top-0 z-10 flex h-17 items-center justify-between bg-black px-4">
         <Image src="/logo.svg" alt="서비스 로고" width={98} height={28} priority />
 
-        <div className="align-center flex gap-4">
-          <button type="button">
-            <AlarmDefault className="h-6 w-6 flex-none text-gray-50" />
-          </button>
-          <button type="button" onClick={handleMenuClick}>
-            {isMenuOpen ? (
-              <X className="h-6 w-6 flex-none text-white" />
-            ) : (
-              <Menu className="h-6 w-6 flex-none text-gray-50" />
-            )}
-          </button>
-        </div>
+        {isLoggedIn && (
+          <div className="align-center flex gap-4">
+            <button type="button">
+              <AlarmDefault className="h-6 w-6 flex-none text-gray-50" />
+            </button>
+            <button type="button" onClick={handleMenuClick}>
+              {shouldShowMenu ? (
+                <X className="h-6 w-6 flex-none text-white" />
+              ) : (
+                <Menu className="h-6 w-6 flex-none text-gray-50" />
+              )}
+            </button>
+          </div>
+        )}
       </div>
 
-      {isMenuOpen && (
+      {shouldShowMenu && (
         <>
           <div className="fixed inset-0 z-20" onClick={handleMenuClick} />
           <div className="text-body-16m text-gray-10 fixed top-17 z-20 flex w-full flex-col gap-6 bg-black px-6 py-5">
